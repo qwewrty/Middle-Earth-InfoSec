6b4638950b661e166e1f86937bba53b9a986cca2
Sandeep S <sandeep.carmel@gmail.com>
2020-12-07 07:34:06 -0500

    Added various functionalities to netcat

--- netcat.py
+++ netcat.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 
-import sys, socket, threading
+import sys, socket, threading, subprocess
+
+# def setNonBlocking(fd):
+#     """
+#     Set the file description of the given file descriptor to non-blocking.
+#     """
+#     flags = fcntl.fcntl(fd, fcntl.F_GETFL)
+#     flags = flags | os.O_NONBLOCK
+#     fcntl.fcntl(fd, fcntl.F_SETFL, flags)
 
 class writingThread (threading.Thread):
     def __init__(self, sock):
@@ -18,76 +26,104 @@ class writingThread (threading.Thread):
         self._isRunning = False
     
     def readAll(self):
-        print("Ready to read:")
-        buffer=b''
+        # print("Ready to read:")
+        # buffer=b''
         while self._isRunning:
-            data = self.sock.recv(1024)
-            buffer += data
-            if b'\n' in buffer:
-                print(buffer.decode('utf-8'), end='')
-                buffer=b''
+            data = self.sock.recv(1)
+            # buffer += data
+            # if b'\n' in buffer:
+            #     sys.stdout.write(buffer.decode('utf-8'))
+                # buffer=b''
             if not data:
                 break
-        print(buffer.decode('utf-8'))
+            sys.stdout.write(data.decode('utf-8'))
+        # sys.stdout.write(buffer.decode('utf-8'))
         
 
-def connect(hostToConnect='', portToConnect=-1):
+def connect(hostToConnect='', portToConnect=-1, command=None):
     # Get host ip and port to connect to
     host = socket.gethostbyname(hostToConnect if hostToConnect != '' else input("Enter the host to connect to: "))
     port = portToConnect if portToConnect!=-1 else int(input("Enter port to connect to: "))
+    proc = None
+
+    print("Connecting to " + host + ":"+str(port))
+    # Does not work
+    # if file!=None:
+    #     sys.stdout = file
+    #     sys.stdin = file
+
+    if command!=None:
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, bufsize=1, universal_newlines=True)
+        sys.stdout = proc.stdin
+        sys.stdin = proc.stdout
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     socket.timeout(5)
     # Connect to the host and port provided
     connected = s.connect_ex((host, port))
     if connected == 0:
-        print("Connected...!")
+        sys.stdout.write("Connected...!\n")
     else:
-        print("Unable to connect error "+ str(connected))
+        sys.stderr.write("Unable to connect error "+ str(connected) + "\n")
         sys.exit()
     # Start up the listener thread to print received packets onto stdout.
     opThread = writingThread(s)
+    opThread.setDaemon(True)
     opThread.start()
     # Start sending anything written into stdin to the connected host.
     try:
         while 1:
-            ip = input()
-            s.send((ip+"\r\n").encode('utf-8'))
-            # writingThread(s).start()
-    except KeyboardInterrupt:
+            ip = sys.stdin.readline()
+            s.send((ip).encode('utf-8'))
+    except (KeyboardInterrupt, ConnectionAbortedError):
         # Incase of an interrupt stop the thread and quit
-        print("Quitting...")
+        sys.stdout.write("Quitting...")
         s.shutdown(socket.SHUT_WR)
         opThread.terminate()
-        sys.exit()
-    except ConnectionAbortedError:
-        s.shutdown(socket.SHUT_WR)
-        opThread.terminate()
+        if proc!= None:
+            proc.terminate()
         sys.exit()
     except Exception as err:
-        print("oops: "+str(err))
+        sys.stderr.write("oops: "+str(err))
 
-def listen(portToListen=-1):
+def listen(portToListen=-1, command=None):
+    # Initialize listener on localhost
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     host = socket.gethostbyname("localhost")
     
+    # Get port to listen on
     port = portToListen if portToListen != -1 else int(input("Enter port to listen on: "))
 
+    print("Listening on " + str(port))
+
+    # Bind and start lisening on port
     serverSocket.bind((host, port))
     serverSocket.listen(3)
+
+    # Startup the subprocess command if provided
+    if command!=None:
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, bufsize=1, universal_newlines=True)
+        sys.stdout = proc.stdin
+        sys.stdin = proc.stdout
+
+    # Continue once we have the connection
     clientSocket, address = serverSocket.accept()
-    print("Connected to ", str(address))
+    sys.stdout.write("Connected to "+ str(address) + "\n")
+
+    # Start up the listener thread to print received packets onto stdout.
     opThread = writingThread(clientSocket)
+    opThread.setDaemon(True)
     opThread.start()
 
+    # Start sending anything written into stdin to the connected host.
     try:
         while 1:
-            ip = input()
-            clientSocket.send((ip+"\r\n").encode('utf-8'))
+            ip = sys.stdin.readline()
+            clientSocket.send((ip).encode('utf-8'))
     except KeyboardInterrupt:
         # Incase of an interrupt stop the thread and quit
-        print("Quitting...")
+        sys.stdout.write("Quitting...")
         clientSocket.shutdown(socket.SHUT_WR)
         opThread.terminate()
         sys.exit()
@@ -96,9 +132,27 @@ def listen(portToListen=-1):
         opThread.terminate()
         sys.exit()
     except Exception as err:
-        print("oops: "+str(err))
+        sys.stderr.write("oops: "+str(err))
     
 
-# connect([])
+def triggerModule(opts):
+    port = -1
+    isListener = False
+    host=''
+    command=None
+    for opt, arg in opts:
+        if opt in ("-l","--listen"):
+            isListener = True
+        elif opt in ("-p", "--port"):
+            port = int(arg)
+        elif opt in ("-t", "--host"):
+            host = arg
+        elif opt in ("-c", "--command"):
+            command = [arg.split(" ")]
+    if isListener:
+        listen(port, command)
+    else:
+        connect(host, port, command)
+# connect("localhost", 9876, ["nc localhost 9000"])
 # listen([])
     
